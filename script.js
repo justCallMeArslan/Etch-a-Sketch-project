d9d344ab5c6cd9ff462e1f3c4d35bbd0c037e9f3
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-01-17 18:20:22 +0300

    Add erase button. Refactor.

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 // DOM references
 const mainContainer = document.querySelector("#container");
-const cell = document.querySelector(".singleDiv");
 const newSketchButton = document.querySelector(".newGrid");
+const whiteColor = document.querySelector(".white");
+const blackColor = document.querySelector(".black");
+const redColor = document.querySelector(".red");
+const rgbColor = document.querySelector(".RGB");
+const eraseGrid = document.querySelector(".erase");
+
+let currentColor = "black";
+let currentGridSize = 10;
+
+whiteColor.addEventListener("click", () => {
+    currentColor = "white"
+});
+blackColor.addEventListener("click", () => {
+    currentColor = "black"
+});
+redColor.addEventListener("click", () => {
+    currentColor = "red"
+});
+rgbColor.addEventListener("click", () => {
+    currentColor = "rgb"
+})
+
+
 
 let isMouseDown = false;
 document.addEventListener("mousedown", () => {
@@ -15,37 +37,25 @@ document.addEventListener("mouseup", () => {
 
 // creating divs using DOM
 function createGrid(columns) {
-    mainContainer.replaceChildren(); // clear any existing grid cells (e.g while 
-    // creating new grid as per user input)
+    mainContainer.replaceChildren(); // clear any existing grid cells (e.g while creating new grid)
 
-    const containerSize = 750;  // same as container's height in CSS
-    const rows = columns; // making grid square, more readable
-    const singleDivSize = containerSize / columns; // divides height of each element equally
+    const containerSize = 750; // #container height
+    const rows = columns; // set grid to square
+    const singleDivSize = containerSize / columns; // as grid set to square
 
 
-    for (let i = 0; i < columns * rows; i++) { // we set 10 columns , rows = columns, so to fully 
-        // fill container we need maximum of 100 boxes, i<100 
+    for (let i = 0; i < columns * rows; i++) {
         const singleDiv = document.createElement("div");
         singleDiv.classList.add("singleDiv");
-        // set singleDiv size dynamically
-        singleDiv.style.width = `${750 / columns}px`; // dividing 100% width to numbers of columns to 
-        // fill all boxes precisely in % of total width
-        singleDiv.style.height = `${singleDivSize}px`; // same as width but for height in px
-
-        // setting up effect to paint each singleDiv
-
-        const randomColor = `rgb(
-        ${Math.floor(Math.random() * 256)},
-        ${Math.floor(Math.random() * 256)}, 
-        ${Math.floor(Math.random() * 256)})`;
-
+        singleDiv.style.width = `${singleDivSize}px`;
+        singleDiv.style.height = `${singleDivSize}px`;
 
         singleDiv.addEventListener("mousedown", () => {
-            singleDiv.style.backgroundColor = randomColor;
+            singleDiv.style.backgroundColor = setColor();
         });
         singleDiv.addEventListener("mouseover", () => {
             if (isMouseDown) {
-                singleDiv.style.backgroundColor = randomColor;
+                singleDiv.style.backgroundColor = setColor();
             }
         });
 
@@ -53,16 +63,28 @@ function createGrid(columns) {
     };
 };
 
-createGrid(10);
+createGrid(currentGridSize);
 
-function createNewSketch() {
-    newSketchButton.addEventListener("click", () => {
-        const userRequest = +prompt("How many columns do you want?", "10");
-        if (userRequest < 1 || userRequest > 100 || isNaN(userRequest)) {
-            alert("Wrong input, please use number between 1 and 100");
-        }
-        createGrid(userRequest);
-    });
-};
+newSketchButton.addEventListener("click", () => {
+    const userRequest = +prompt("How many columns do you want?", "10");
+    if (userRequest < 1 || userRequest > 100 || !Number.isInteger(userRequest)) {
+        alert("Wrong input, please use number between 1 and 100");
+        return;
+    };
+    currentGridSize = userRequest;
+    createGrid(currentGridSize);
+});
 
-createNewSketch();
+eraseGrid.addEventListener("click", () => {
+    createGrid(currentGridSize);
+})
+
+function setColor() {
+    if (currentColor === "rgb") {
+        return `rgb(
+        ${Math.floor(Math.random() * 256)},
+        ${Math.floor(Math.random() * 256)}, 
+        ${Math.floor(Math.random() * 256)})`;
+    }
+    return currentColor;
+}
