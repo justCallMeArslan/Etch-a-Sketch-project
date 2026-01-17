43281892ffac11f8604b91caa9c5cda6881fbe35
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-01-17 13:34:56 +0300

    Complete painting feature using DOM.

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 // DOM references
 const mainContainer = document.querySelector("#container");
+const cell = document.querySelector(".singleDiv");
 
-
+let isMouseDown = false;
+document.addEventListener("mousedown", () => {
+    isMouseDown = true;
+});
+document.addEventListener("mouseup", () => {
+    isMouseDown = false;
+})
 
 // creating divs using DOM
 function createGrid(columns) {
@@ -9,7 +16,7 @@ function createGrid(columns) {
     // creating new grid as per user input)
 
     const containerSize = 960;  // same as container's height in CSS
-    const rows = columns; // making grid square
+    const rows = columns; // making grid square, more readable
     const singleDivSize = containerSize / columns; // divides height of each element equally
 
 
@@ -17,15 +24,35 @@ function createGrid(columns) {
         // fill container we need maximum of 100 boxes, i<100 
         const singleDiv = document.createElement("div");
         singleDiv.classList.add("singleDiv");
-
         // set singleDiv size dynamically
         singleDiv.style.width = `${960 / columns}px`; // dividing 100% width to numbers of columns to 
         // fill all boxes precisely in % of total width
         singleDiv.style.height = `${singleDivSize}px`; // same as width but for height in px
+
+        // setting up effect to paint each singleDiv
+
+        const randomColor = `rgb(
+        ${Math.floor(Math.random() * 256)},
+        ${Math.floor(Math.random() * 256)}, 
+        ${Math.floor(Math.random() * 256)})`;
+
+
+        singleDiv.addEventListener("mousedown", () => {
+            singleDiv.style.backgroundColor = randomColor;
+        })
+        singleDiv.addEventListener("mouseover", () => {
+            if (isMouseDown) {
+                singleDiv.style.backgroundColor = randomColor;
+            }
+        })
+
+        singleDiv.addEventListener('click', () => {
+            singleDiv.style.backgroundColor = "black";
+        })
 
         mainContainer.appendChild(singleDiv);
 
     }
 }
 
-createGrid(16);
+createGrid(25);
